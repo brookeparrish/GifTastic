8d1cb042a1301afff9341b29a8406f0e21d42c21
Brooke <parrish.e.brooke@gmail.com>
2019-09-25 19:59:18 -0700

    added in some js

--- giftastic.js
+++ giftastic.js
@@ -1,3 +1,50 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    //declaring inital array of topics which is tv shows
+    let topics = ["The Office", "Riverdale", "The Ranch", "Stranger Things", "Grey's Anatomy", "Criminal Minds", "Law & Order: Special Victims Unit"];
+
+    //loop to go through array and append buttons
+    for (let i = 0; i < topics.length; i++) {
+        let buttons = $("<button>" + topics[i] + "</button>")
+        buttons.appendTo("#topics");
+    }
+
+    $("button").on("click", function () {
+        let tvShow = $(this).attr("data-tvShow");
+        let queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=f0j6tZXu8gyQYNHWKYatmQHRLfu96jlO&q=tv show&limit=10";
+
+        $ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            console.log(queryURL);
+
+            console.log(response);
+
+            let results = response.data;
+
+            for (let i = 0; i < results.length; i++) {
+
+                let tvShowDiv = $("<div>");
+                let p = $("<p>").text("Rating: " + results[i].rating);
+
+                let tvShowImage = $("<img>");
+                tvShowImage.attr("src", results[i].images.fixed_height.url);
+
+                tvShowDiv.append(p);
+                tvShowDiv.append(tvShowImage);
+
+                $("#gifs-appear-here").prepend(tvShowDiv);
+            }
+        });
+    });
+    
+
+
+    
+    
+    //function to display info on topics by using gipfy api
+   
 
 });
+
