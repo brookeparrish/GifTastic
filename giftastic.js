ecd93fcae9c067f64471e1785e4f56846a121dc8
Brooke <parrish.e.brooke@gmail.com>
2019-09-27 16:34:11 -0700

    made changes and got the submit button to add new button for gif search

--- giftastic.js
+++ giftastic.js
@@ -1,57 +1,43 @@
 $(document).ready(function () {
     //declaring inital array of topics which is tv shows
     let topics = ["The Office", "Riverdale", "The Ranch", "Stranger Things", "The Simpsons", "Criminal Minds", "Rugrats"];
-
     //loop to go through array and append buttons
     for (let i = 0; i < topics.length; i++) {
         let buttons = $(`<button value="${topics[i]}">${topics[i]}</button>`);
-
+        buttons.addClass("btn")
         buttons.appendTo("#topics");
     }
-
-    $("button").on("click", function () {
-        let tvShow = $(this).attr("data-tvShow");
+    // onclick for top buttons
+    $(document).on("click", ".btn", function () {
+        // let tvShow = $(this).attr("data-tvShow");
+        $("#gifs-appear-here").empty();
         let searchStr = encodeURI($(this).val());
         let queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=f0j6tZXu8gyQYNHWKYatmQHRLfu96jlO&q=" + searchStr + "&limit=10&lang=en";
-
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-        .then(function(response) {
-            console.log(queryURL);
-
-            console.log(response);
-
-            let results = response.data;
-
-            for (let i = 0; i < results.length; i++) {
-
-                let tvShowDiv = $("<div>");
-                let p = $("<p>").text("Rating: " + results[i].rating);
-
-                let tvShowImage = $("<img>");
-                tvShowImage.attr("src", results[i].images.fixed_height_still.url);
-                tvShowImage.attr("class", "gif");
-                tvShowImage.attr("data-still", results[i].images.fixed_height_still.url);
-                tvShowImage.attr("data-animate", results[i].images.fixed_height.url);
-                tvShowImage.attr("data-state", "still");
-
-
-                tvShowDiv.append(p);
-                tvShowDiv.append(tvShowImage);
-
-               $("#gifs-appear-here").prepend(tvShowDiv);
-
-               
-
-            }
-        })
+            .then(function (response) {
+                console.log(queryURL);
+                console.log(response);
+                let results = response.data;
+                for (let i = 0; i < results.length; i++) {
+                    let tvShowDiv = $("<div>");
+                    let p = $("<p>").text("Rating: " + results[i].rating);
+                    let tvShowImage = $("<img>");
+                    tvShowImage.attr("src", results[i].images.fixed_height_still.url);
+                    tvShowImage.attr("class", "gif");
+                    tvShowImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    tvShowImage.attr("data-animate", results[i].images.fixed_height.url);
+                    tvShowImage.attr("data-state", "still");
+                    tvShowDiv.append(p);
+                    tvShowDiv.append(tvShowImage);
+                    $("#gifs-appear-here").prepend(tvShowDiv);
+                }
+            })
     })
-
-    $(document).on("click", ".gif", function() {
+    $(document).on("click", ".gif", function () {
         let state = $(this).attr("data-state");
-
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
@@ -60,30 +46,26 @@ $(document).ready(function () {
             $(this).attr("data-state", "still");
         }
     })
-
-
-
-
-
-        $(document).on("click", ".btn", function(event){
-            event.preventDefault();
-
-            let input = $("#tvShow-input").val().trim();
-            topics.push(input);
-            $(`<button value="${topics[i]}">${topics[i]}</button>`);
-
-            buttons.appendTo("#topics");
-            
-        });
+    // submit button onclick
+    $(document).on("click", ".btn-primary", function (event) {
+        event.preventDefault();
+        let input = $("#tvShow-input").val().trim();
+        topics.push(input);
+        let addedBtn = $(`<button value="${input}"></button>`);
+        addedBtn.addClass("btn");
+        addedBtn.text(input);
+        // $(`<button value="${topics[i]}">${topics[i]}</button>`);
+        // buttons.appendTo("#topics");
+        $("#topics").append(addedBtn);
     });
-
-   
-
-    
+});
 
 
-    
 
-   
+
+
+
+
+
 
 
